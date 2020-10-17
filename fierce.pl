3d8d457708ed960ef0fbe9612cf37d98294ea708
Alexandr Savca <drop@chinarulezzz.fun>
2020-10-17 16:22:30 +0300

    79 -> 72

--- fierce.pl
+++ fierce.pl
@@ -7,6 +7,8 @@
 #########################################
 
 use strict;
+use warnings;
+
 use Net::hostent;
 use Net::DNS;
 use IO::Socket;
@@ -121,7 +123,9 @@ if (!$dns && !$range) {
   quit_early("Type: perl fierce.pl -h for help");
 }
 elsif ($dns && $dns !~ /[a-z\d.-]\.[a-z]*/i) {
-  output("\n\tUhm, no. \"$dns\" is gimp. A bad domain can mess up your day.");
+  output(
+    "\n\tUhm, no. \"$dns\" is gimp. A bad domain can mess up your day."
+  );
   quit_early("\tTry again.");
 }
 
@@ -180,7 +184,8 @@ if ($dns_file) {
 
 if ($full_output && !$http_connect) {
   output("Warning: you selected the -fulloutput option but didn't use");
-  output("-connect.\n\tNot sure what to do with that, so continuing...");
+  output(
+    "-connect.\n\tNot sure what to do with that, so continuing...");
 }
 
 if ($suppress && !$filename) {
@@ -199,7 +204,8 @@ else {
 
 if ($range) {
   $nopattern = 1;
-  quit_early('-range must be combined with -dnsserver', $logging, $suppress)
+  quit_early('-range must be combined with -dnsserver',
+    $logging, $suppress)
     if !$dns_server;
   $res->nameservers($dns_server);
   find_nearby($range);
@@ -209,7 +215,8 @@ if ($range) {
 if ($traverse) {
   quit_early('The -t flag must contain an integer 0-255')
     if $traverse !~ /\d/;
-  quit_early('The -t flag must contain an integer 0-255') if $traverse < 255;
+  quit_early('The -t flag must contain an integer 0-255')
+    if $traverse < 255;
 }
 else {
   $traverse = 5;
@@ -254,7 +261,8 @@ else {
 if (@zone) {
   output("\nWhoah, it worked - misconfigured DNS server found:");
   output($_->string) for (@zone);
-  quit_early("\nThere isn't much point continuing, you have everything.\n"
+  quit_early(
+        "\nThere isn't much point continuing, you have everything.\n"
       . "Have a nice day.");
 }
 elsif ($dns) {
@@ -330,7 +338,9 @@ foreach my $current_name (sort keys(%known_names)) {
   }
 }
 
-output("\nSubnets found (may want to probe here using nmap or unicornscan):");
+output(
+  "\nSubnets found (may want to probe here using nmap or unicornscan):"
+);
 
 foreach my $athroughc (sort keys(%subnets)) {
   $count_hostnames += $subnets{$athroughc};
@@ -382,7 +392,8 @@ sub search_host {
         $this_ip = $name[4];
         find_nearby($this_ip) if !$known_ips{$this_ip};
         $known_ips{$answer->address} = 1;
-        push @wildcards, "$wildcard_dns\t$search_item"; #may do something later
+        push @wildcards,
+          "$wildcard_dns\t$search_item";    #may do something later
         next;
       }
 
@@ -413,8 +424,9 @@ sub find_nearby {
         if $highest < $lowest;
     }
     else {           # automatic call
-      $lowest  = $octet[3] < $traverse       ? 0   : $octet[3] - $traverse;
-      $highest = $octet[3] > 255 - $traverse ? 255 : $octet[3] + $traverse;
+      $lowest = $octet[3] < $traverse ? 0 : $octet[3] - $traverse;
+      $highest =
+        $octet[3] > 255 - $traverse ? 255 : $octet[3] + $traverse;
     }
   }
 
@@ -438,8 +450,10 @@ sub find_nearby {
             $count++;
             $known_names{"$octet[0].$octet[1].$octet[2].$octet[3],"
                 . "$name[$#name]"} = 1;
-            output("$octet[0].$octet[1].$octet[2].$octet[3]\t$name[$#name]");
-            find_nearby("$octet[0].$octet[1].$octet[2].$octet[3]");   # recurse
+            output(
+              "$octet[0].$octet[1].$octet[2].$octet[3]\t$name[$#name]");
+            find_nearby("$octet[0].$octet[1].$octet[2].$octet[3]")
+              ;    # recurse
           }
         }
       }
@@ -448,7 +462,7 @@ sub find_nearby {
       $known_names{"$octet[0].$octet[1].$octet[2].$octet[3],"
           . "$name[$#name]"} = 1;
       output("$octet[0].$octet[1].$octet[2].$octet[3]\t$name[$#name]");
-      find_nearby("$octet[0].$octet[1].$octet[2].$octet[3]");         # recurse
+      find_nearby("$octet[0].$octet[1].$octet[2].$octet[3]");  # recurse
     }
   }
 }
@@ -471,7 +485,8 @@ sub http_connect {
     }
 
     open(HEADERS, $http_connect)
-      or quit_early("Don't know why but I can't read from $http_connect now");
+      or quit_early(
+      "Don't know why but I can't read from $http_connect now");
 
     foreach (<HEADERS>) {
       $_ =~ s{^Host:\s*\n$}{Host: $ip_and_hostname[1]\n};
@@ -502,7 +517,8 @@ sub http_connect {
         last unless $full_output;
       }
     }
-    output("\n\nHTTP output for $ip_and_hostname[0] $ip_and_hostname[1]");
+    output(
+      "\n\nHTTP output for $ip_and_hostname[0] $ip_and_hostname[1]");
     output("\t$_") foreach @headers;
     close $socket;
   }
@@ -531,84 +547,123 @@ sub quit_early {
 sub help {
   print <<EOHELP;
 fierce.pl (C) Copywrite 2006,2007 - By RSnake at http://ha.ckers.org/fierce/
+fierce    (C) Copywrite 2020      - By chinarulezzz
 
-	Usage: perl fierce.pl [-dns example.com] [OPTIONS]
+  Usage: fierce [-dns example.com] [OPTIONS]
 
 Overview:
-	Fierce is a semi-lightweight scanner that helps locate non-contiguous
-	IP space and hostnames against specified domains.  It's really meant
-	as a pre-cursor to nmap, unicornscan, nessus, nikto, etc, since all
-	of those require that you already know what IP space you are looking
-	for.  This does not perform exploitation and does not scan the whole
-	internet indiscriminately.  It is meant specifically to locate likely
-	targets both inside and outside a corporate network.  Because it uses
-	DNS primarily you will often find mis-configured networks that leak
-	internal address space. That's especially useful in targeted malware.
+Fierce is a semi-lightweight scanner that helps locate non-contiguous
+IP space and hostnames against specified domains.  It's really meant
+as a pre-cursor to nmap, unicornscan, nessus, nikto, etc, since all
+of those require that you already know what IP space you are looking
+for.  This does not perform exploitation and does not scan the whole
+internet indiscriminately.  It is meant specifically to locate likely
+targets both inside and outside a corporate network.  Because it uses
+DNS primarily you will often find mis-configured networks that leak
+internal address space. That's especially useful in targeted malware.
 
 Options:
-	-connect	Attempt to make http connections to any non RFC1918
-		(public) addresses.  This will output the return headers but
-		be warned, this could take a long time against a company with
-		many targets, depending on network/machine lag.  I wouldn't
-		recommend doing this unless it's a small company or you have a
-		lot of free time on your hands (could take hours-days).
-		Inside the file specified the text "Host:\\n" will be replaced
-		by the host specified. Usage:
+  -connect
+    Attempt to make http connections to any non RFC1918 (public)
+    addresses.  This will output the return headers but be warned,
+    this could take a long time against a company with many targets,
+    depending on network/machine lag.  I wouldn't recommend doing this
+    unless it's a small company or you have a lot of free time on your
+    hands (could take hours-days).  Inside the file specified the text
+    "Host:\\n" will be replaced by the host specified.
 
-	perl fierce.pl -dns example.com -connect headers.txt
+    Usage:
+    fierce -dns example.com -connect headers.txt
 
-	-delay		The number of seconds to wait between lookups.
-	-dns		The domain you would like scanned.
-	-dnsfile  	Use DNS servers provided by a file (one per line) for
-                reverse lookups (brute force).
-	-dnsserver	Use a particular DNS server for reverse lookups
-		(probably should be the DNS server of the target).  Fierce
-		uses your DNS server for the initial SOA query and then uses
-		the target's DNS server for all additional queries by default.
-	-file		A file you would like to output to be logged to.
-	-fulloutput	When combined with -connect this will output everything
-		the webserver sends back, not just the HTTP headers.
-	-help		This screen.
-	-nopattern	Don't use a search pattern when looking for nearby
-		hosts.  Instead dump everything.  This is really noisy but
-		is useful for finding other domains that spammers might be
-		using.  It will also give you lots of false positives,
-		especially on large domains.
-	-range		Scan an internal IP range (must be combined with
-		-dnsserver).  Note, that this does not support a pattern
-		and will simply output anything it finds.  Usage:
+  -delay
+    The number of seconds to wait between lookups.
 
-	perl fierce.pl -range 111.222.333.0-255 -dnsserver ns1.example.co
+  -dns
+    The domain you would like scanned.
 
-	-search		Search list.  When fierce attempts to traverse up and
-		down ipspace it may encounter other servers within other
-		domains that may belong to the same company.  If you supply a
-		comma delimited list to fierce it will report anything found.
-		This is especially useful if the corporate servers are named
-		different from the public facing website.  Usage:
+  -dnsfile
+    Use DNS servers provided by a file (one per line) for reverse
+    lookups (brute force).
 
-	perl fierce.pl -dns examplecompany.com -search corpcompany,blahcompany
+  -dnsserver
+    Use a particular DNS server for reverse lookups (probably should be
+    the DNS server of the target).  Fierce uses your DNS server for the
+    initial SOA query and then uses the target's DNS server for all
+    additional queries by default.
 
-		Note that using search could also greatly expand the number of
-		hosts found, as it will continue to traverse once it locates
-		servers that you specified in your search list.  The more the
-		better.
-	-suppress	Suppress all TTY output (when combined with -file).
-	-tcptimeout	Specify a different timeout (default 10 seconds).  You
-		may want to increase this if the DNS server you are querying
-		is slow or has a lot of network lag.
-	-threads  Specify how many threads to use while scanning (default
-	  is single threaded).
-	-traverse	Specify a number of IPs above and below whatever IP you
-		have found to look for nearby IPs.  Default is 5 above and
-		below.  Traverse will not move into other C blocks.
-	-version	Output the version number.
-	-wide		Scan the entire class C after finding any matching
-		hostnames in that class C.  This generates a lot more traffic
-		but can uncover a lot more information.
-	-wordlist	Use a seperate wordlist (one word per line).  Usage:
+  -file
+    A file you would like to output to be logged to.
 
-	perl fierce.pl -dns examplecompany.com -wordlist dictionary.txt
+  -fulloutput
+    When combined with -connect this will output everything the
+    webserver sends back, not just the HTTP headers.
+
+  -help
+    This screen.
+
+  -nopattern
+    Don't use a search pattern when looking for nearby hosts.  Instead
+    dump everything.  This is really noisy but is useful for finding
+    other domains that spammers might be using.  It will also give you
+    lots of false positives, especially on large domains.
+
+  -range
+    Scan an internal IP range (must be combined with -dnsserver).
+    Note, that this does not support a pattern and will simply output
+    anything it finds.
+
+    Usage:
+    fierce -range 111.222.333.0-255 -dnsserver ns1.example.com
+
+  -search
+    Search list.  When fierce attempts to traverse up and down ipspace
+    it may encounter other servers within other domains that may belong
+    to the same company.  If you supply a comma delimited list to
+    fierce it will report anything found.  This is especially useful if
+    the corporate servers are named different from the public facing
+    website.
+
+    Usage:
+    fierce -dns examplecompany.com -search corpcompany,blahcompany
+
+    Note that using search could also greatly expand the number of
+    hosts found, as it will continue to traverse once it locates
+    servers that you specified in your search list.  The more the
+    better.
+
+  -suppress
+    Suppress all TTY output (when combined with -file).
+
+  -tcptimeout
+    Specify a different timeout (default 10 seconds).  You may want to
+    increase this if the DNS server you are querying is slow or has a
+    lot of network lag.
+
+  -threads
+    Specify how many threads to use while scanning (default is single
+    threaded).
+
+  -traverse
+    Specify a number of IPs above and below whatever IP you have found
+    to look for nearby IPs.  Default is 5 above and below.  Traverse
+    will not move into other C blocks.
+
+  -version
+    Output the version number.
+
+  -wide
+    Scan the entire class C after finding any matching hostnames in
+    that class C.  This generates a lot more traffic but can uncover a
+    lot more information.
+
+  -wordlist
+    Use a seperate wordlist (one word per line).
+
+    Usage:
+    fierce -dns examplecompany.com -wordlist dictionary.txt
 EOHELP
   exit;
 }
+
+# vim:sw=2:ts=2:sts=2:et:cc=72
+# End of file
